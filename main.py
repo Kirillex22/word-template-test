cca1d96323f5d29287dc7e098d759c86159bbe10
Kirillex22 <ziabrew34@gmail.com>
2026-01-09 23:39:00 +0600

    linux fix

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     print("API документация: http://localhost:8000/docs")
     print("=" * 60)
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
